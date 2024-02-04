74648e5443c518f58bb30d9fae6510cb6af49188
Anubha Pant <pant.anubha24@gmail.com>
2024-02-04 15:24:11 +0530

    wip: navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,5 @@
-const { default: Link } = require("next/link");
+import styles from "./links.module.css";
+import NavLink from "./navLink/NavLink";
 
 const Links = () => {
   const links = [
@@ -20,11 +21,9 @@ const Links = () => {
     },
   ];
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
+        <NavLink item={link} key={link.title} />
       ))}
     </div>
   );
