e09dbb522cdd6f2b19c9128a3edbbc1ee29d5688
Anubha Pant <pant.anubha24@gmail.com>
2024-02-07 15:44:46 +0530

    added assets and navbar functionality with media query

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import styles from "./about.module.css";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.unsplash.com/photo-1682686581797-21ec383ead02?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="about"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,7 +1,10 @@
+"use client";
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
   const links = [
     {
       title: "Home",
@@ -20,11 +23,38 @@ const Links = () => {
       path: "/blog",
     },
   ];
+
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
