cd5f62ee9a0e743fde9b9ab0d1875b3de89b8321
Anubha Pant <pant.anubha24@gmail.com>
2024-02-09 15:21:06 +0530

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,37 @@
 import Image from "next/image";
 import styles from "./about.module.css";
+
 const About = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precision. We are world's
+          special best team consulting and finance olution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10K+</h1>
+            <p>Years of Experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>243K+</h1>
+            <p>People reached</p>
+          </div>
+          <div className={styles.box}>
+            <h1>5K+</h1>
+            <p>Services and plugin</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image
-          src="https://images.unsplash.com/photo-1682686581797-21ec383ead02?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="about"
-          fill
-        />
+        <Image src="/about.png" alt="About Page" fill className={styles.img} />
       </div>
     </div>
   );
