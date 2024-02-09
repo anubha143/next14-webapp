8cb5b70c8f38318e4cdd58ee4fc82b5c348f69d6
Anubha Pant <pant.anubha24@gmail.com>
2024-02-09 14:46:02 +0530

    home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,29 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 const Home = () => {
   // throw new Error("Error in Home");
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thought Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          iusto, aliquam delectus omnis voluptatum, beatae, maxime dolorem at
+          iure eveniet consequatur voluptates accusamus? Ipsam, animi?
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,13 @@
+import styles from "./footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Anubha</div>
+      <div className={styles.text}>
+        Anubha creative thoughts agency &copy; All rights reserved
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
